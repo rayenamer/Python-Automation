ab16fc281c3d55f161765e75499ef5f925a5cee8
rayenamer <rayen.ameur@esprit.tn>
2025-02-05 19:15:25 +0100

    Day 5.1

---
+++ PythonAutomationFoundations/AutomatingEmails/SendSingleEmail.py
@@ -0,0 +1,25 @@
+import yagmail
+
+sender = 'rayenamer@aiesec.net'
+receiver = 'vopugyvusif3@mimimail.me'
+
+subject = 'This is the subject'
+
+content = """
+i closed the stmp connection manually without the help of chatgbt :D
+"""
+
+password = input("gimmi dat password baby ->")
+
+# Initialize yagmail SMTP object
+yag = yagmail.SMTP(user=sender, password=password)
+
+# Send the email
+yag.send(to=receiver, subject=subject, contents=content)
+
+# Print confirmation
+print("email sent!")
+
+yag.close()
+
+print("connection closed")
